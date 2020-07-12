741107da92a528a9827dc84951453d11b2e14cb8
M Algah Fattah Illahi <herppratama@gmail.com>
2020-07-12 11:12:53 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+// axios.get('https://api.nim.aryuuu.ninja/get/nim/ardy')
+// .then((response) => {
+//   console.log(response.data);
+// })
+// .catch(err => console.log(err))
+const getMhs = async () => {
+  const response = await axios.get('https://api.nim.aryuuu.ninja/get/nim/ardy');
+  console.log(response.data);
+}
+getMhs();
